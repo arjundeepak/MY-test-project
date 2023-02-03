57f2822bcb6036ee35e57d9def24e2a9d97c9067
arjundeepak <kdeepakarjun246@gmail.com>
2023-02-03 22:11:50 +0530

    bash update

--- git-pull.sh
+++ git-pull.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 
+# repo path to pull
+
+cd /e/bash
+
+git branch
+# repo url
+
+repo-url=https://github-ci-token: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@github.com/arjundeepak/MY-test-project.git/git-pull.sh
+
+git pull
